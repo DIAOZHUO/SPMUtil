6192396f1eed94f3b083c9998219f8b5932efcb1
ZHUO DIAO <41063862+DIAOZHUO@users.noreply.github.com>
2021-11-01 15:40:07 +0900

    init

---
+++ SPMUtil/__init__.py
@@ -0,0 +1,8 @@
+from SPMUtil._flatten import *
+from SPMUtil.filters import filter_1d, filter_2d
+import SPMUtil._formula as formula
+import SPMUtil._structures as structures
+import SPMUtil.converter as converter
+from SPMUtil.DataSerializer import DataSerializer
+
+
