05e03e7656f5c6889797d6d839470f3e8db6d9f7
kyoka <41063862+DIAOZHUO@users.noreply.github.com>
2021-11-26 16:20:27 +0900

    Update _structures.py

--- SPMUtil/_structures.py
+++ SPMUtil/_structures.py
@@ -7,7 +7,7 @@ class Rect2D:
     def __str__(self):
         return "left_bottom:" + str(self.points) + ", window_size: (" + str(self.xbox) + ", " + str(self.ybox) + ")"
 
-    def __init__(self, points=(0,0), xbox=1, ybox=1):
+    def __init__(self, points=(0,0), xbox=1, ybox=1, round_random=False):
         # if points[0] < 0:
         #     points[0] = 0
         # if points[1] < 0:
@@ -17,16 +17,34 @@ class Rect2D:
         if ybox < 1:
             ybox = 1
         self.points = (int(points[0]), int(points[1]))
-        self.xbox = int(xbox)
-        self.ybox = int(ybox)
+        if round_random:
+            if self.__roll(xbox - np.floor(xbox)):
+                self.xbox = int(np.ceil(xbox))
+            else:
+                self.xbox = int(np.floor(xbox))
+        if round_random:
+            if self.__roll(ybox - np.floor(ybox)):
+                self.ybox = int(np.ceil(ybox))
+            else:
+                self.ybox = int(np.floor(ybox))
+        else:
+            self.xbox = int(xbox)
+            self.ybox = int(ybox)
 
+    def __roll(self, percent=0.5):
+        return percent > np.random.rand()
+
+
+    @property
+    def center(self):
+        return (int(self.points[0] + self.xbox/2), int(self.points[1] + self.ybox/2))
 
     def extract_2d_map_from_rect(self, map: np.ndarray):
         x_point, y_point = self.points[0], self.points[1]
         return map[y_point:y_point+self.ybox, x_point:x_point+self.xbox]
 
-    def draw_rect_patch_on_matplot(self, ax):
-        _rect = Rectangle(self.points, self.xbox, self.ybox, linewidth=1, edgecolor="r", facecolor="none")
+    def draw_rect_patch_on_matplot(self, ax, c="r"):
+        _rect = Rectangle(self.points, self.xbox, self.ybox, linewidth=1, edgecolor=c, facecolor="none")
         ax.add_patch(_rect)
 
     @staticmethod
